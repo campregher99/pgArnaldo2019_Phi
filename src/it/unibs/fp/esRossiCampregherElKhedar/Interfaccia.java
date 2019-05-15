c911d9356f762adbbc21a73156e949ae06a102b4
campregher99 <f.campregher@studenti.unibs.it>
2019-05-15 19:18:18 +0200

    first commit

---
+++ src/it/unibs/fp/esRossiCampregherElKhedar/Interfaccia.java
@@ -0,0 +1,48 @@
+package it.unibs.fp.esRossiCampregherElKhedar;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Interfaccia {
+	/**
+	 * 
+	 */
+	private Scanner lettore = new Scanner(System.in);
+
+	/**
+	 * 
+	 * @param msg
+	 * @return
+	 */
+	public int letturaInt(String msg) {
+		boolean finito = false;
+		int valoreLetto = 0;
+		do {
+			System.out.print(msg);
+			try {
+				valoreLetto = lettore.nextInt();
+				finito = true;
+			} catch (InputMismatchException e) {
+				System.out.println("il valore inserito non è nel formato corretto");
+				lettore.next();
+			}
+		} while (!finito);
+		return valoreLetto;
+	}
+
+	/**
+	 * 
+	 * @param msg
+	 */
+	public void scriviR(String msg) {
+		System.out.println(msg);
+	}
+
+	/**
+	 * 
+	 * @param msg
+	 */
+	public void scriviC(String msg) {
+		System.out.print(msg);
+	}
+}
